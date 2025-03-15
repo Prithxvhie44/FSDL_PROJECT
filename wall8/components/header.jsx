1eafe826a7945af4188bd47269b8b00a85d3a0be
Prithxvhie44 <44prithviraj44@gmail.com>
2025-03-15 17:53:20 +0530

    Header fixed

--- wall8/components/header.jsx
+++ wall8/components/header.jsx
@@ -1,25 +1,56 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
 
 const Header = () => {
-  return (
-    <div className="fixed top-0 ">
-      <nav>
-        <link  href="/"/>
-        <Image/>
-      </nav>
-      {/* <header className="flex justify-end items-center p-4 gap-4 h-16"> */}
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      {/* </header> */}
-    </div>
-  );
+    return (
+        <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+            <nav className="container mx-auto px-4 py-4 flex  items-center justify-between">
+                <Link href="/" />
+                <Image src={"/logo.png"}
+                    alt="logo"
+                    width={200}
+                    height={60}
+                    className="h-12 w-auto object-contain"
+                />
+
+                <div className="flex items-center space-x-4">
+                    <SignedIn>
+                        <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2 ">
+                            <Button variant="outline">
+                                <LayoutDashboard size={18} className="w-6 h-6 mr-2" />
+                                <span className="hidden md:inline">Dashboard</span></Button>
+                        </Link>
+
+                        <Link href={"/transaction/create"} >
+                            <Button  className=" flex items-center gap-2">
+                                <PenBox size={18} className="w-6 h-6 mr-2" />
+                                <span className="hidden md:inline">Add Transaction </span></Button>
+                        </Link>
+                    </SignedIn>
+
+
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl="/dashboard" >
+                            <Button variant="outline">Login </Button>
+                        </SignInButton>
+                        <SignUpButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton 
+                        appearance={{
+                            elements:{
+                                avatarBox: "w-10 h-10",
+                            }
+                        }}/>
+                    </SignedIn>
+                </div>
+            </nav>
+        </div>
+    );
 };
 
 export default Header;
