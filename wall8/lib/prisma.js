ef442f984bdb850184567d64d0aed89e773ed2d9
Prithxvhie44 <44prithviraj44@gmail.com>
2025-03-16 15:44:09 +0530

    Done till routing small errors fixed

--- wall8/lib/prisma.js
+++ wall8/lib/prisma.js
@@ -1,3 +1,6 @@
-import {} from '@prisma/'
+import {PrismaClient} from '@prisma/client';
 
-export const db = globalThis.prisma || new Prismaclient
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') 
+    {globalThis.prisma = db;}
