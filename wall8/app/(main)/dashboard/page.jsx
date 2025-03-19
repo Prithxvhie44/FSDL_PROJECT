ca57b196c5087432593e3b6c5bd810ba7a245724
Prithxvhie44 <44prithviraj44@gmail.com>
2025-03-19 23:27:44 +0530

    Done till 2.32 account switching still redirects to accounts/

--- wall8/app/(main)/dashboard/page.jsx
+++ wall8/app/(main)/dashboard/page.jsx
@@ -1,9 +1,13 @@
+import { getUserAccounts } from '@/actions/dashboard'
 import CreateAccountDrawer from '@/components/create-account-drawer'
 import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import React from 'react'
+import AccountCard from './_components/account-card'
 
-function DashboardPage(){
+async function DashboardPage(){
+  const accounts = await getUserAccounts()
+    
 
 
     return<div className='px-5 '>
@@ -22,6 +26,10 @@ function DashboardPage(){
             </CardContent>
           </Card>
         </CreateAccountDrawer>
+
+        {accounts.length > 0 && accounts?.map((account) => {
+          return <AccountCard key={account.id} account={account} />
+        })}
         
       </div>
     </div>
